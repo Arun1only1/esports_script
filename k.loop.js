e21c894c6c626be5509b08cb20569ae13e393007
Arun1only1 <arun1nly1@gmail.com>
2024-01-11 13:24:52 +0545

    loop

---
+++ k.loop.js
@@ -0,0 +1,69 @@
+// loop
+// iteration => repetition
+
+// for
+// while
+// do... while
+
+// for
+// syntax
+
+// for (initial_value; condition; increment / decrement) {
+// some code here
+// }
+
+// loop => 10 times
+
+// for (let i = 0; i <= 3; i = i + 1) {
+//   console.log("Hi", i);
+// }
+
+// WAP to print values from 1 to 10
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//   console.log(i);
+// }
+
+// 10 to 1
+
+// for (let i = 10; i >= 1; i = i - 1) {
+//   console.log(i);
+// }
+
+// infinite loop => loop that does not terminate
+
+// for (let i = 100; i >= 50; i = i - 2) {
+//   console.log(i);
+// }
+
+// for (let i = 30; i <= 100; i = i + 1) {
+//   let remainder = i % 2;
+
+//   if (remainder === 0) {
+//     console.log(`${i} is even`);
+//   } else {
+//     console.log(`${i} is odd`);
+//   }
+// }
+
+// ? WAP to print sum of numbers from 1 to 10
+
+let sum = 0;
+for (let i = 1; i <= 10; i = i + 1) {
+  sum = sum + i;
+}
+
+console.log(sum);
+
+// ? WAP to print product of numbers from 1 to 5
