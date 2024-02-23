9b722daac36eaad3031c42dca0c0f14ab8eca0cf
Arun1only1 <arun1nly1@gmail.com>
2024-02-23 13:54:45 +0545

    promises and async/await

---
+++ z4.error.handling.js
@@ -0,0 +1,15 @@
+// error handling
+
+console.log("Hi");
+const x = 5;
+
+try {
+  x = 7;
+} catch (error) {
+  console.log(error.message);
+}
+
+let firstName = "Rohan";
+console.log(`My friends name is ${firstName}`);
+
+console.log("I am from Nepal");
